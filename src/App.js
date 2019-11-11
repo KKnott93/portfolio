d6a7ffd9c5efe8aa9e3fc41b45cb23fe9d9d102b
Kevin Knott <KKnoTT93@gmail.com>
2019-11-10 20:49:30 -0600

    First Component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Kevin, I am a software engineer.</p>
+        <p>
+          I am creating a React App to help build my portfolio and learn even
+          more!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
