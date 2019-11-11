f21c0a99fdf8a24d5d2c1386ed307c4633b72d02
Kevin Knott <KKnoTT93@gmail.com>
2019-11-10 21:39:34 -0600

    Created state and methods to change state (not best practice)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+    // console.log("Component more this", this);
+
+    this.readMore = this.readMore.bind(this);
+    this.readLess = this.readLess.bind(this);
+  }
+
+  readMore() {
+    // console.log("your button was clicked");
+    this.setState({ displayBio: true });
+  }
+  readLess() {
+    // console.log("your button was clicked");
+    this.setState({ displayBio: false });
+  }
+
   render() {
+    // turnary operator makes this a constant instead of a variable (noted by let)
+    const bio = this.state.displayBio ? (
+      <div>
+        <p>I am currently living in the greater DFW metroplex in Texas</p>
+        <p>My favorite coding languages are</p>
+        <ol type="1">
+          <li>Python</li>
+          <li>Golang</li>
+          <li>Java</li>
+        </ol>
+        <p>I also enjoy Swimming, WaterPolo, Rock Climbing, and Traveling!</p>
+
+        <button onClick={this.readLess}>Read Less</button>
+      </div>
+    ) : (
+      <div>
+        <button onClick={this.readMore}>Read More</button>
+      </div>
+    );
+
+    // Basic if check for bio state
+    // if (!this.state.displayBio) {
+    //     bio = null;
+    // }
+
     return (
       <div>
         <h1>Hello!</h1>
@@ -10,6 +53,9 @@ class App extends Component {
           I am creating a React App to help build my portfolio and learn even
           more!
         </p>
+
+        {/* You can add code within {} */}
+        {bio}
       </div>
     );
   }
