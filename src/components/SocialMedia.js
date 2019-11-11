8d89265b83b070a8a6bd78fed81674292d80e702
Kevin Knott <KKnoTT93@gmail.com>
2019-11-11 16:50:26 -0600

    Moving to stateless functional components

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,47 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import SOCIAL_PROFILES from "../data/socialProfiles";
 
-class SocialProfile extends Component {
-  render() {
-    // console.log("this.props", this.props);
+const SocialProfile = (props) => {
+  // console.log("this.props", this.props);
+  const { link, image } = props.socialProfile;
 
-    const { link, image } = this.props.socialProfile;
+  return (
+    <span>
+      <a href={link}>
+        <img
+          src={image}
+          alt="social-profile"
+          style={{
+            width: 35,
+            height: 35,
+            margin: 10
+          }}
+        />
+      </a>
+    </span>
+  );
+}
 
-    return (
-      <span>
-        <a href={link}>
-          <img
-            src={image}
-            alt="social-profile"
-            style={{
-              width: 35,
-              height: 35,
-              margin: 10
-            }}
+
+const SocialProfiles = () => (
+  <div>
+    <h2>Reach out to me!</h2>
+    <div>
+      {SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
+        return (
+          <SocialProfile
+            key={SOCIAL_PROFILE.id}
+            socialProfile={SOCIAL_PROFILE}
           />
-        </a>
-      </span>
-    );
-  }
-}
+        );
+      })}
+    </div>
+  </div>
+)
 
-class SocialProfiles extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Reach out to me!</h2>
-        <div>
-          {SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
-            return (
-              <SocialProfile
-                key={SOCIAL_PROFILE.id}
-                socialProfile={SOCIAL_PROFILE}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
-}
 export default SocialProfiles;
